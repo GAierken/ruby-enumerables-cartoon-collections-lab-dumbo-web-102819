b72dbc5960c8e09af5a24129a2d67ca1d810794a
Guligena Aierken <guligena_aierken@163.com>
2019-10-23 03:35:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,8 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese (array)
-    cheese = ["cheddar", "gouda", "camembert"]
+    cheese_list = ["cheddar", "gouda", "camembert"]
     array.find do |snack|
-      cheese.include?(snack)
+      cheese_list.include?(snack)
     end
 end
